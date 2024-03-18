455257f627ef7b5c9aaf650023fd3db2964b1eaf
divysuthar <divysuthar029@gmail.com>
2024-03-18 22:05:54 +0530

    Uploading

---
+++ base/app/urls.py
@@ -0,0 +1,17 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutPage, name="logout"),
+    path('register/', views.registerPage, name="register"),
+    path('room/<str:pk>/', views.room, name='room'),
+    path('userprofile/<str:pk>/', views.userProfile, name='user-profile'),
+    path('create-room/', views.createRoom, name='create-room'),
+    path('update-room/<str:pk>', views.updateRoom, name='update-room'),
+    path('delete-room/<str:pk>', views.deleteRoom, name='delete-room'),
+    path('delete-message/<str:pk>', views.deleteMessage, name='delete-message'),
+]
